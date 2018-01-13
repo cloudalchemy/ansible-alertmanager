e11bf8c0daaa683b9b9a4aea58b06499ab6e396f
paulfantom <pawel@krupa.net.pl>
2018-01-13 18:08:30 +0100

    refactor tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,51 +1,38 @@
+#!/user/bin/env python
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_directories(File):
-    present = [
+def test_directories(host):
+    dirs = [
         "/opt/alertmanager",
         "/etc/alertmanager",
         "/etc/alertmanager/templates",
         "/var/lib/alertmanager",
         "/var/log/alertmanager"
     ]
-    if present:
-        for directory in present:
-            d = File(directory)
-            assert d.is_directory
-            assert d.exists
-
-
-def test_files(File):
-    present = [
+    files = [
         "/opt/alertmanager/alertmanager",
         "/etc/alertmanager/alertmanager.yml",
         "/etc/systemd/system/alertmanager.service"
     ]
-    if present:
-        for file in present:
-            f = File(file)
-            assert f.exists
-            assert f.is_file
+    for directory in dirs:
+        d = host.file(directory)
+        assert d.is_directory
+        assert d.exists
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
 
 
-def test_service(Service):
-    present = [
-        "alertmanager"
-    ]
-    if present:
-        for service in present:
-            s = Service(service)
-            assert s.is_enabled
-            assert s.is_running
+def test_service(host):
+    s = host.service("alertmanager")
+    assert s.is_enabled
+    assert s.is_running
 
 
-def test_socket(Socket):
-    present = [
-        "tcp://127.0.0.1:9093"
-    ]
-    for socket in present:
-        s = Socket(socket)
-        assert s.is_listening
+def test_socket(host):
+    s = host.socket("tcp://127.0.0.1:9093")
+    assert s.is_listening
